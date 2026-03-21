c54b9edcde74f98b112c831ad42c3c8166c2d7b1
karmakondakeerthana <karmakondakeerthana1904@gmail.com>
2026-03-21 18:04:30 +0530

    Create increment submatrices by one.py

---
+++ increment submatrices by one.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        diff = [[0] * (n + 1) for _ in range(n + 1)]
+        for r1, c1, r2, c2 in queries:
+            diff[r1][c1] += 1
+            diff[r1][c2 + 1] -= 1
+            diff[r2 + 1][c1] -= 1
+            diff[r2 + 1][c2 + 1] += 1
+        for i in range(n):
+            for j in range(n):
+                diff[i][j] += (diff[i - 1][j] if i > 0 else 0) + \
+                              (diff[i][j - 1] if j > 0 else 0) - \
+                              (diff[i - 1][j - 1] if i > 0 and j > 0 else 0)
+        res = [row[:n] for row in diff[:n]]
+        return res

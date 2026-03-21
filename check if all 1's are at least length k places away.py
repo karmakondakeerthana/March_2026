a39cdb4186d3229e479fc2cb8fc8c8913f42c69b
karmakondakeerthana <karmakondakeerthana1904@gmail.com>
2026-03-21 18:11:06 +0530

    Create check if all 1's are at least length k places away.py

---
+++ check if all 1's are at least length k places away.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        last = -1 
+        for i, val in enumerate(nums):
+            if val == 1:
+                if last != -1 and i - last - 1 < k:
+                    return False
+                last = i 
+        return True

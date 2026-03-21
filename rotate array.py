aabb95139d728601d83acdf1d1560c4a7585714b
karmakondakeerthana <karmakondakeerthana1904@gmail.com>
2026-03-21 18:05:52 +0530

    Create rotate array.py

---
+++ rotate array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n 
+        def reverse(nums, left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        reverse(nums, 0, n - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, n - 1)

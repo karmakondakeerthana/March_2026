65bd0e52eb447dc33794ba7c0116a63d899f2bb5
karmakondakeerthana <karmakondakeerthana1904@gmail.com>
2026-03-23 19:04:02 +0530

    Create paths in matrix whose sum is divisible by k.py

---
+++ paths in matrix whose sum is divisible by k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
+        MOD = 10**9 + 7
+        m, n = len(grid), len(grid[0])
+        dp = [[[0] * k for _ in range(n)] for _ in range(m)]
+        dp[0][0][grid[0][0] % k] = 1
+        for i in range(m):
+            for j in range(n):
+                for r in range(k):
+                    if i > 0:
+                        prev_r = (r - grid[i][j] % k + k) % k
+                        dp[i][j][r] = (dp[i][j][r] + dp[i-1][j][prev_r]) % MOD
+                    if j > 0:
+                        prev_r = (r - grid[i][j] % k + k) % k
+                        dp[i][j][r] = (dp[i][j][r] + dp[i][j-1][prev_r]) % MOD
+        return dp[m-1][n-1][0]

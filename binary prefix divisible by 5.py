b052cf63172e759cb74fb8da176fc20caac5dba5
karmakondakeerthana <karmakondakeerthana1904@gmail.com>
2026-03-23 18:58:52 +0530

    Create binary prefix divisible by 5.py

---
+++ binary prefix divisible by 5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        answer = []
+        remainder = 0
+        for num in nums:
+            remainder = (remainder * 2 + num) % 5
+            answer.append(remainder == 0)
+        return answer
